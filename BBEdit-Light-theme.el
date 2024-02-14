663d4342f2a121f604b751fb15541a4a0cd9e217
dilluti0n <hskimse1@gmail.com>
2024-02-14 19:54:16 +0900

    Add header

---
+++ BBEdit-Light-theme.el
@@ -0,0 +1,5 @@
+;; BBEdit-Light-theme.el - port of BBEdit 11 default theme to emacs
+;; Author: Dilluti0n <hskimse1@gmail.com>
+;; URL: https://github.com/dilluti0n/emacs-BBEdit-Light-theme
+;; Version:
+

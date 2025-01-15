a0a4ca90fba1f03a5f18cf52bdfcff0a15ff9b62
dilluti0n <hskimse1@gmail.com>
2025-01-15 21:51:36 +0900

    Add mode-line face

--- BBEdit-Light-theme.el
+++ BBEdit-Light-theme.el
@@ -1,10 +1,10 @@
-;; BBEdit-Light-theme.el - port of BBEdit 11 default theme to emacs
+;; BBEdit-Light-theme.el - Port of BBEdit 11 default theme to emacs
 ;; Author: Dilluti0n <hskimse1@gmail.com>
 ;; URL: https://github.com/dilluti0n/emacs-BBEdit-Light-theme
-;; Version: 0.1.1
+;; Version: 0.2.0
 
 (deftheme BBEdit-Light
-  "port of BBEdit 11 default color scheme to emacs.")
+  "Port of BBEdit 11 default colorscheme to emacs.")
 
 (let ((bg            "#f7f7f7")
       (fg+2          "#636363")
@@ -22,33 +22,52 @@
       (PPline        "#d02325")
       (PPdirective   "#2738b0")
       (constants     "#346ab9")
-      (line-num-bg   "#d2d2d2")
-      )
-(custom-theme-set-faces
- 'BBEdit-Light
- `(default ((t (:foreground ,fg :background ,bg))))
- `(cursor ((t (:background ,fg))))
- `(font-lock-comment-face ((t (:foreground ,comments))))
- `(font-lock-constant-face ((t (:foreground ,constants))))
- `(font-lock-function-name-face ((t (:foreground ,fg))))
- `(font-lock-keyword-face ((t (:foreground ,keyword))))
- `(font-lock-number-face ((t (:foreground ,number))))
- `(font-lock-operator-face ((t (:foreground ,keyword))))
- `(font-lock-string-face ((t (:foreground ,string))))
- `(font-lock-escape-face ((t (:foreground ,string))))
- `(font-lock-regexp-face ((t (:foreground ,string))))
- `(font-lock-variable-name-face ((t (:foreground ,fg))))
- `(font-lock-preprocessor-face ((t (:foreground ,keyword))))
- `(font-lock-type-face ((t (:foreground ,keyword))))
- ;; for highlight-numbers package
- `(highlight-numbers-number ((t (:foreground ,number))))
- ;; regions...
- `(hl-line ((t (:background ,lineHighlight))))
- `(region ((t (:background ,selection :foreground nil))))
- ;; line-numbers
- `(line-number-current-line ((t (:background ,line-num-bg :foreground ,fg))))
- `(line-number ((t (:foreground ,fg+2))))
- )
-)
+      (line-num-curr-bg   "#d2d2d2")
+      (line-num-bg "#eaeaea")
+      (mode-line-fg "#535353")
+      (mode-line-bg "#d4d4d4")
+      (mode-line-bg-2 "#e6e6e6"))
+  (custom-theme-set-faces
+   'BBEdit-Light
+
+   ;; standard
+
+   `(default ((t (:foreground ,fg :background ,bg))))
+   `(region ((t (:background ,selection :foreground nil))))
+   `(cursor ((t (:background ,fg))))
+   `(mode-line ((t (:background ,mode-line-bg :foreground ,mode-line-fg :box nil))))
+   `(mode-line-inactive ((t (:background ,mode-line-bg-2 :foreground ,mode-line-fg :box nil))))
+
+   ;; font-lock
+
+   `(font-lock-comment-face ((t (:foreground ,comments))))
+   `(font-lock-constant-face ((t (:foreground ,constants))))
+   `(font-lock-builtin-face ((t (:foreground ,PPdirective))))
+   `(font-lock-keyword-face ((t (:foreground ,keyword))))
+   `(font-lock-number-face ((t (:foreground ,number))))
+   `(font-lock-operator-face ((t (:foreground ,keyword))))
+   `(font-lock-string-face ((t (:foreground ,string))))
+   `(font-lock-escape-face ((t (:foreground ,string))))
+   `(font-lock-regexp-face ((t (:foreground ,string))))
+   `(font-lock-variable-name-face ((t (:foreground ,fg))))
+   `(font-lock-preprocessor-face ((t (:foreground ,keyword))))
+   `(font-lock-type-face ((t (:foreground ,keyword))))
+
+   ;; highlight-numbers
+
+   `(highlight-numbers-number ((t (:foreground ,number))))
+
+   ;; hl-line
+
+   `(hl-line ((t (:background ,lineHighlight))))
+
+   ;; line-numbers
+
+   `(line-number-current-line ((t (:background ,line-num-curr-bg :foreground ,fg))))
+   `(line-number ((t (:background ,line-num-bg :foreground ,fg+2))))
+
+   )
+  )
+
 
 (provide-theme 'BBEdit-Light)
